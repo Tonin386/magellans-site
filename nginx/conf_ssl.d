d6940a9f747b62c33542e6fb36e7a621361e69a0
Tonin386 <tonin386@gmail.com>
2023-11-26 12:22:13 +0100

    Updated SSL configuration

--- nginx/conf_ssl.d
+++ nginx/conf_ssl.d
@@ -3,20 +3,7 @@ server {
     listen [::]:80;
     server_name magellans.fr;
 
-    location /static/ {
-        alias /app/staticfiles/;
-    }
-
-    location / {
-        proxy_set_header X-Forwarded-Host $host;
-        proxy_set_header X-Forwarded-Server $host;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-
-        access_log /var/log/nginx/access_backend.log;
-        error_log /var/log/nginx/error_backend.log;
-		proxy_pass http://django:8000;
-	}
+    return 301 https://magellans.fr;
 }
 
 server {
