0cbd8ea344c7784def5712847d978b53b242fb32
Tonin386 <tonin386@gmail.com>
2024-01-30 19:28:52 +0100

    Fixed first version of front page. Backend updates.

--- app/assets/js/home.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let titleDiv = document.querySelector('#title');
-    titleDiv.classList.add('loaded');
-
-    let logos = document.querySelectorAll('.img-fluid');
-    logos.forEach(logo => {
-        logo.classList.add('loaded')
-    });
-})
