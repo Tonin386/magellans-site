6cd946c243d3a14e5fef9fadf0987bed67dff9be
Tonin386 <tonin386@gmail.com>
2024-02-05 20:57:49 +0100

    Try v2

--- nginx/conf_ssl.d
+++ nginx/conf_ssl.d
@@ -61,7 +61,7 @@ server {
     ssl_certificate_key /app/ssl/privkey.pem;
 
     location / {
-        proxy_pass http://roundcube:80
+        proxy_pass http://roundcube:80;
         proxy_set_header X-Forwarded-Host $host;
         proxy_set_header X-Forwarded-Server $host;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
