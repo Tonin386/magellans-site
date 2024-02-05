8b8c794615f6907f523ed00a09d0a09e37ca2d3b
Tonin386 <tonin386@gmail.com>
2024-02-05 20:52:14 +0100

    First try setting up roundcube service

--- nginx/conf_ssl.d
+++ nginx/conf_ssl.d
@@ -1,3 +1,4 @@
+#Config for the main website
 server {
     listen 80;
     listen [::]:80;
@@ -40,4 +41,63 @@ server {
         proxy_set_header Connection "upgrade";
         proxy_read_timeout 86400;
     }
+}
+
+#Config for every mail related service
+server {
+    listen 80;
+    listen [::]:80;
+    server_name mail.magellans.fr;
+
+    return 301 https://mail.magellans.fr;
+}
+
+server {
+    listen 443 ssl;
+    listen [::]:443 ssl;
+    server_name mail.magellans.fr;
+
+    ssl_certificate /app/ssl/fullchain.pem;
+    ssl_certificate_key /app/ssl/privkey.pem;
+
+    location / {
+        proxy_pass http://roundcube:80
+        proxy_set_header X-Forwarded-Host $host;
+        proxy_set_header X-Forwarded-Server $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+
+        access_log /var/log/nginx/access_backend.log;
+        error_log /var/log/nginx/error_backend.log;
+    }
+}
+
+server {
+    listen 587;
+    server_name mail.magellans.fr;
+
+    location / {
+        proxy_pass http://mail-server:587;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+
+    # Additional configuration for your SMTP server if needed...
+}
+
+server {
+    listen 993;
+    server_name mail.magellans.fr;
+
+    location / {
+        proxy_pass http://mail-server:993;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+
+    # Additional configuration for your IMAP server if needed...
 }
--- run.sh
+++ run.sh
@@ -7,16 +7,16 @@ fi
 
 if [ "$1" = "up" ]; then
   echo "Starting my django app."
-  docker-compose --env-file app/.env up -d
+  docker compose --env-file app/.env up -d
 elif [ "$1" = "log" ]; then
-  docker-compose --env-file app/.env up
+  docker compose --env-file app/.env up
 elif [ "$1" = "down" ]; then
-  docker-compose --env-file app/.env down
+  docker compose --env-file app/.env down
 elif [ "$1" = "in" ]; then
-  docker-compose --env-file app/.env up -d
+  docker compose --env-file app/.env up -d
   docker exec -it django-app sh
 elif [ "$1" = "db" ]; then
-  docker-compose --env-file app/.env up -d
+  docker compose --env-file app/.env up -d
   docker exec -it django-app python manage.py makemigrations
   docker exec -it django-app python manage.py migrate
 else
