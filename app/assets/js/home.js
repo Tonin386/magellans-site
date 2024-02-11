bed8455668b6ddc42a351731b02cc49ee6bfbeaa
Tonin386 <tonin386@gmail.com>
2024-02-11 18:19:55 +0100

    New version for the front page. Trying to setup mailserver

--- app/assets/js/home.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let panel = document.querySelector("#main-panel");
-    panel.style.height = (window.innerHeight - document.querySelector("#title").clientHeight) + 'px'
-});
--- app/showcase/views.py
+++ app/showcase/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from showcase.forms import ContactForm
+from django.conf import settings
+from .forms import ContactForm
 
 def home(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            phone = form.cleaned_data['phone']
+            message = form.cleaned_data['message']
+
+            subject = 'Contact Form Submission'
+            message_body = f'Name: {name}\nEmail: {email}\nPhone: {phone}\n\nMessage:\n{message}'
+            sender_email = settings.EMAIL_HOST_USER
+            recipient_list = [settings.EMAIL_RECEIVER]
+
+            print(subject, message_body, sender_email, recipient_list)
+    else:
+        form = ContactForm()
+    
     if request.user.is_authenticated:
         redirect('/membres')
         
