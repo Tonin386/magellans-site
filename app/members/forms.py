f6d6faf7fc9f86ff3e4f22f06576b13dcd4f2ccc
Tonin386 <tonin386@gmail.com>
2024-03-09 14:58:47 +0100

    Created register handles

--- app/members/forms.py
+++ app/members/forms.py
@@ -1,4 +1,21 @@
 from django import forms
+from .models import Member
+from django_registration.forms import RegistrationForm
 
 class LogoutForm(forms.Form):
     pass
+
+class RegisterForm(RegistrationForm):
+    
+    class Meta:
+        model = Member
+        fields = [
+            'email',
+            'first_name',
+            'last_name',
+            'phone',
+            'gender'
+        ]
+        
+    def save(self, commit=True):
+        return super(RegisterForm, self).save(commit=commit)
--- app/members/urls.py
+++ app/members/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', members, name='members'),
     path('<int:pk>/', MemberDetailView.as_view(), name='member-detail'),
     path('mon-profil', my_profile, name="my-profile"),
+    path('inscription/', register, name="register"),
+    path('activation-reussie', activation_done, name="activation_done"),
+    path('activation-echec', activation_failed, name='activation_failed')
 ]
