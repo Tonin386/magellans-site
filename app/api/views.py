0f67704c1a64547dbb944efc3315c0fb6b99a003
Tonin386 <tonin386@gmail.com>
2024-04-09 00:31:24 +0200

    Fixed wrong call for member

--- app/api/views.py
+++ app/api/views.py
@@ -36,13 +36,13 @@ def verify_token_permissions(request):
         permissions['dashboard'] += ['read', 'write']
         permissions['members'] += ['read', 'write']
         
-        if user.site_profile.role == "G":
+        if user.site_person.role == "G":
             permissions['warehouse'] += ['write']
             
-        if user.site_profile.role == "P":
+        if user.site_person.role == "P":
             permissions['bank'] += ['read']
             
-        if user.site_profile.role == "T":
+        if user.site_person.role == "T":
             permissions['bank'] += ['read', 'write']
         
     if user.is_superuser:
