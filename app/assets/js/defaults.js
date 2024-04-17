3e64ebc939484a7f3a43bacffddd1f15352101f9
Tonin386 <tonin386@gmail.com>
2024-04-17 14:33:11 +0200

    Added custom description meta tag for every page. Added responsive dynamic footer to every page.

--- app/assets/js/defaults.js
+++ app/assets/js/defaults.js
@@ -1,13 +1,13 @@
 var monthFrNames = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
 function convertFrDate(str_date) {
-
+    
     let parts = str_date.split(' ');
     let day = parseInt(parts[0], 10);
     let month = monthFrNames.indexOf(parts[1]);
     let year = parseInt(parts[2], 10);
-
+    
     let date = new Date(year, month, day);
-
+    
     return date;
 }
 
@@ -19,9 +19,9 @@ function convertFrDatetime(str_datetime) {
     let time = parts[3].split(':');
     let hour = parseInt(time[0], 10);
     let minute = parseInt(time[1], 10);
-
+    
     let date = new Date(year, month, day, hour, minute);
-
+    
     return date;
 }
 
@@ -55,48 +55,81 @@ $.extend($.fn.dataTableExt.oSort, {
         
         return numB - numA;
     },
-
+    
     "person-desc": function(a, b) {
         let numA = parseInt(a.replace("@", "99999"), 10);
         let numB = parseInt(b.replace("@", "99999"), 10);
-
+        
         return numB - numA;
     },
-
+    
     "person-asc": function(b, a) {
         let numA = parseInt(a.replace("@", "99999"), 10);
         let numB = parseInt(b.replace("@", "99999"), 10);
-
+        
         return numB - numA;
     },
-
+    
     "amount-desc": function(a, b) {
         var numA = parseFloat(a.replace('€', ''), 10);
         var numB = parseFloat(b.replace('€', ''), 10);
-
+        
         return numB - numA;
     },
-
+    
     "amount-asc": function(b, a) {
         var numA = parseFloat(a.replace('€', ''), 10);
         var numB = parseFloat(b.replace('€', ''), 10);
-
+        
         return numB - numA;
     },
-
+    
     "frdatetime-desc": function(a, b) {
         return convertFrDatetime(b).getTime() - convertFrDatetime(a).getTime();
     },
-
+    
     "frdatetime-asc": function(b, a) {
         return convertFrDatetime(b).getTime() - convertFrDatetime(a).getTime();
     },
-
+    
     "frdate-desc": function(a, b) {
         return convertFrDate(b).getTime() - convertFrDate(a).getTime();
     },
-
+    
     "frdate-asc": function(b, a) {
         return convertFrDate(b).getTime() - convertFrDate(a).getTime();
     },
 });
+
+function defineStickyButtonsBehavior(heightBefore) {
+    const scrollPosition = window.scrollY;
+    
+    if(scrollPosition + window.innerHeight + footer.offsetHeight * 0.5 >= heightBefore) {
+        stickyButtons.classList.add("sticky-bottom-buttons-scrollDone");
+        stickyButtons.classList.remove("sticky-bottom-buttons");
+    } else {
+        stickyButtons.classList.remove("sticky-bottom-buttons-scrollDone");
+        stickyButtons.classList.add("sticky-bottom-buttons");
+    }
+}
+
+$(window).on('load', function(){
+    let footer = document.querySelector("footer");
+    
+    if (document.body.clientHeight < window.outerHeight) {
+        footer.classList.add("footer-fixed");
+    }
+    
+    const heightBefore = document.body.scrollHeight;
+    
+    let stickyButtons = document.querySelector(".sticky-bottom-buttons");
+    if(stickyButtons !== null) {
+        
+        defineStickyButtonsBehavior(heightBefore);
+        
+        window.addEventListener("scroll", function() {
+            defineStickyButtonsBehavior(heightBefore);
+        });
+    }
+});
+
