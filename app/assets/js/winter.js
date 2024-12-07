9b99e9440216f3dfde28dc5ecc21922ffcfc266b
Tonin386 <tonin386@gmail.com>
2024-12-07 14:52:52 +0100

    Fixed winter theme

--- app/assets/js/winter.js
+++ app/assets/js/winter.js
@@ -6,8 +6,8 @@ for (let i = 0; i < numFlakes; i++) {
     const snowFlake = document.createElement('div');
     snowFlake.classList.add('snow', 'text-white');
     snowFlake.textContent = '*';  // Caractère représentant la neige
-    snowFlake.style.left = `${Math.random() * 100}vw`;  // Position aléatoire sur l'axe X
+    snowFlake.style.left = `${Math.random() * 99}vw`;  // Position aléatoire sur l'axe X
     snowFlake.style.animationDuration = `${Math.random() * 3 + 7.5}s`;  // Durée aléatoire de la chute
-    snowFlake.style.animationDelay = `${Math.random() * 5}s`;  // Retard aléatoire avant de commencer la chute
+    snowFlake.style.animationDelay = `${Math.random() * 10}s`;  // Retard aléatoire avant de commencer la chute
     snowContainer.appendChild(snowFlake);
 }
